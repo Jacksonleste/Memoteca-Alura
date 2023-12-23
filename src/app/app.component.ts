940411b779304591d66b9a76bf2bf34465af186d
Jackson Leste <jacksongoncalves.leste@gmail.com>
2023-12-22 21:07:09 -0300

    Feat: crioacao do rodape

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CabecalhoComponent],
+  imports: [CommonModule, RouterOutlet, CabecalhoComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
